18feb91f5bf0facb9a9790215182fa11880decf7
ahrar01 <ahraralam2012@gmail.com>
2019-09-03 02:13:17 +0530

    Phone auth done

--- app/src/main/java/com/ahraar/friendsnearby/Activity/Login.java
+++ app/src/main/java/com/ahraar/friendsnearby/Activity/Login.java
@@ -1,16 +1,153 @@
 package com.ahraar.friendsnearby.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.ahraar.friendsnearby.OTP.Authentication;
 import com.ahraar.friendsnearby.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private Button googleButton;
+    private Button phoneButton;
+    private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mAuth;
+    private String TAG = "LoginActivity", userID;
+    private int RC_SIGN_IN = 9001;
+    private boolean flag;//True -> goes to next screen, False -> Details Screen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //initialize my Firebase Auth (get an instance of it)
+        mAuth = FirebaseAuth.getInstance();
+        initializeGooglePlusSettings();
+        flag = false;
+        googleButton = (Button) findViewById(R.id.g_plus);
+        phoneButton = (Button) findViewById(R.id.phone);
+        phoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(Login.this, Authentication.class);
+                startActivity(i);
+            }
+        });
+
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                googleSignIn();
+            }
+        });
+        //on create ends here
+    }
+
+    private void googleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        //here we take the account that was passed to this method when the authentication with Gmail was successful, and then use that to perform
+        //a firebase authentication
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            Toast.makeText(Login.this, "Authentication DONE.",
+                                    Toast.LENGTH_SHORT).show();
+
+                            checkLoginStatus();
+                            // startActivity(new Intent(log.this, RegistrationActivity.class));
+                            // finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+
+    private void checkLoginStatus() {
+        FirebaseUser currUser = FirebaseAuth.getInstance().getCurrentUser();
+        String userId = currUser.getUid();
+
+
+    }
+
+
+
+    private void initializeGooglePlusSettings() {
+
+        //intialize the google sign in
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        //get an instance of the google sign in
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
---
+++ app/src/main/java/com/ahraar/friendsnearby/OTP/Authentication.java
@@ -0,0 +1,348 @@
+package com.ahraar.friendsnearby.OTP;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ahraar.friendsnearby.Activity.MainActivity;
+import com.ahraar.friendsnearby.R;
+import com.chaos.view.PinView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.shuhart.stepview.StepView;
+
+import java.util.concurrent.TimeUnit;
+
+import es.dmoral.toasty.Toasty;
+
+public class Authentication extends AppCompatActivity {
+
+    private static final String UNIQUE_ID = "UNIQUE_ID";
+    private static final long ONE_HOUR_MILLI = 60 * 60 * 1000;
+    private static final String TAG = "FirebasePhoneNumAuth";
+    private static String uniqueIdentifier = null;
+    LinearLayout layout1, layout2, layout3;
+    StepView stepView;
+    Dialog dialog_verifying, profile_dialog;
+    private int currentStep = 0;
+    private Spinner spinner;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private FirebaseAuth firebaseAuth;
+
+    private String phoneNumber;
+    private Button sendCodeButton;
+    private Button verifyCodeButton;
+
+    private Button button3;
+
+    private EditText phoneNum;
+    private PinView verifyCodeET;
+    private TextView phonenumberText;
+
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private boolean flag; //True -> goes to next screen, False -> Register Screen
+
+
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference userRef = db.collection("Users");
+
+    String phoneNo;
+
+
+    @SuppressLint("SetTextI18n")
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        flag = false;
+        layout1 = (LinearLayout) findViewById(R.id.layout1);
+        layout2 = (LinearLayout) findViewById(R.id.layout2);
+        layout3 = (LinearLayout) findViewById(R.id.layout3);
+        sendCodeButton = (Button) findViewById(R.id.submit1);
+        verifyCodeButton = (Button) findViewById(R.id.submit2);
+        button3 = (Button) findViewById(R.id.submit3);
+        firebaseAuth = FirebaseAuth.getInstance();
+        phoneNum = (EditText) findViewById(R.id.phonenumber);
+        verifyCodeET = (PinView) findViewById(R.id.pinView);
+        phonenumberText = (TextView) findViewById(R.id.phonenumberText);
+        spinner = findViewById(R.id.spinnerCountries);
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, CountryData.countryNames));
+
+
+        stepView = findViewById(R.id.step_view);
+        stepView.setStepsNumber(3);
+        stepView.go(0, true);
+        layout1.setVisibility(View.VISIBLE);
+        sendCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String code = CountryData.countryAreaCodes[spinner.getSelectedItemPosition()];
+
+                phoneNo = phoneNum.getText().toString();
+                String phoneNumber = "+" + code + phoneNo;
+                phonenumberText.setText(phoneNumber);
+
+                if (TextUtils.isEmpty(phoneNumber)) {
+                    phoneNum.setError("Enter a Phone Number");
+                    phoneNum.requestFocus();
+                } else if (phoneNumber.length() < 10) {
+                    phoneNum.setError("Please enter a valid phone");
+                    phoneNum.requestFocus();
+                } else {
+
+                    if (currentStep < stepView.getStepCount() - 1) {
+                        currentStep++;
+                        stepView.go(currentStep, true);
+                    } else {
+                        stepView.done(true);
+                    }
+                    layout1.setVisibility(View.GONE);
+                    layout2.setVisibility(View.VISIBLE);
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                            phoneNumber,        // Phone number to verify
+                            60,                 // Timeout duration
+                            TimeUnit.SECONDS,   // Unit of timeout
+                            Authentication.this,// Activity (for callback binding)
+                            mCallbacks);        // OnVerificationStateChangedCallbacks
+                }
+            }
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                LayoutInflater inflater = getLayoutInflater();
+                View alertLayout = inflater.inflate(R.layout.dialog_pop_up, null);
+                AlertDialog.Builder show = new AlertDialog.Builder(Authentication.this);
+
+                show.setView(alertLayout);
+                show.setCancelable(false);
+                dialog_verifying = show.create();
+                dialog_verifying.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                dialog_verifying.show();
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+
+            }
+
+            @Override
+            public void onCodeSent(String verificationId,
+                                   PhoneAuthProvider.ForceResendingToken token) {
+
+                // Save verification ID and resending token so we can use them later
+                mVerificationId = verificationId;
+                mResendToken = token;
+
+                // ...
+            }
+        };
+
+        verifyCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String verificationCode = verifyCodeET.getText().toString();
+                if (verificationCode.isEmpty()) {
+                    Toast.makeText(Authentication.this, "Enter verification code", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    LayoutInflater inflater = getLayoutInflater();
+                    View alertLayout = inflater.inflate(R.layout.dialog_pop_up, null);
+                    AlertDialog.Builder show = new AlertDialog.Builder(Authentication.this);
+
+                    show.setView(alertLayout);
+                    show.setCancelable(false);
+                    dialog_verifying = show.create();
+                    dialog_verifying.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                    dialog_verifying.show();
+
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, verificationCode);
+                    signInWithPhoneAuthCredential(credential);
+
+                }
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (currentStep < stepView.getStepCount() - 1) {
+                    currentStep++;
+                    stepView.go(currentStep, true);
+                } else {
+                    stepView.done(true);
+                }
+                LayoutInflater inflater = getLayoutInflater();
+                View alertLayout = inflater.inflate(R.layout.dialog_pop_up, null);
+                AlertDialog.Builder show = new AlertDialog.Builder(Authentication.this);
+                show.setView(alertLayout);
+                show.setCancelable(false);
+                profile_dialog = show.create();
+                profile_dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                profile_dialog.getWindow().setGravity(Gravity.CENTER_HORIZONTAL);
+                profile_dialog.getWindow().setGravity(Gravity.CENTER_VERTICAL);
+                profile_dialog.show();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        FirebaseUser currUser = FirebaseAuth.getInstance().getCurrentUser();
+                        String userId = currUser.getUid();
+                      Intent intent = new Intent(Authentication.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                      //  startActivity(intent);
+
+
+                        profile_dialog.dismiss();
+
+                    }
+                }, 2500);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    private void checkLoginStatus() {
+
+        FirebaseUser currUser = FirebaseAuth.getInstance().getCurrentUser();
+        final String userId = currUser.getUid();
+
+        // Access a Cloud Firestore instance from your Activity
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference users = db.collection("Users");
+
+        Query query = users.whereEqualTo("id", userId);
+        query.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            flag = true;
+                        }
+                        if (flag) {
+                            //get user details
+                            getUserData(userId);
+                        } else {
+                           // startActivity(new Intent(getApplicationContext(), RegistrationPart1.class));
+                        }
+
+
+                    }
+                });
+
+    }
+
+
+    public void getUserData(String userId) {
+        userRef.document(userId).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+
+
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            //Map<String, Object> note = documentSnapshot.getData();
+
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Authentication.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+    }
+
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            dialog_verifying.dismiss();
+                            if (currentStep < stepView.getStepCount() - 1) {
+                                currentStep++;
+                                stepView.go(currentStep, true);
+                            } else {
+                                stepView.done(true);
+                            }
+                            layout1.setVisibility(View.GONE);
+                            layout2.setVisibility(View.GONE);
+                            layout3.setVisibility(View.VISIBLE);
+                            // ...
+                        } else {
+
+                            dialog_verifying.dismiss();
+                            Toasty.error(Authentication.this, "Something wrong", Toast.LENGTH_SHORT).show();
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+
+                            }
+                        }
+                    }
+                });
+    }
+
+
+}
+
